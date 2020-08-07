fd10d7a457b8afa2b2886d930b4f26d89dfb1e29
IanaCris <ianasousati@gmail.com>
2020-08-06 22:32:11 -0300

    page teacher form

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
 
+import './styles.css';
+
 function TeacherForm() {
   return (
     <div id="page-teacher-form" className="container">
-      <PageHeader title="Que incrível que você que dar aulas"/>
+      <PageHeader 
+        title="Que incrível que você que dar aulas"
+        description="O primeiropasso é preencher esse formulário de inscrição"
+      />
+
+      <main>
+        <fieldset>
+          <legend>Seus Dados</legend>
+          <div className="input-block">
+            <label htmlFor="name">Nome Completo</label>
+            <input type="text" id="name"/>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="avatar">Avatar</label>
+            <input type="text" id="avatar"/>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="whatsapp">Whatsapp</label>
+            <input type="text" id="whatsapp"/>
+          </div>
+        </fieldset>
+      </main>
     </div>
   );
 }
